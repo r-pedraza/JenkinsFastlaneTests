d5d7fb474bb51bacde0e042ac7bdef4c95f7cea8
Raul Pedraza <raul.pedraza@ciberexperis.es>
2017-11-14 11:24:27 +0100

    JenkinsFastlaneTests commit

---
+++ JenkinsFastlaneTestsTests/JenkinsFastlaneTestsTests.swift
@@ -0,0 +1,18 @@
+//
+//  JenkinsFastlaneTestsTests.swift
+//  JenkinsFastlaneTestsTests
+//
+//  Created by Raul Pedraza on 14/11/2017.
+//  Copyright © 2017 Raul Pedraza. All rights reserved.
+//
+
+import XCTest
+@testable import JenkinsFastlaneTests
+
+class JenkinsFastlaneTestsTests: XCTestCase {
+    
+    func testExample() {
+        XCTAssertTrue(true)
+    }
+    
+}
